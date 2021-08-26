736601aa0492460de6810747cdff865c652eee36
Poafs1 <poafs1@hotmail.com>
2021-08-26 15:49:13 +0700

    Fix default bug

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,3 @@
-export declare const eventOdd: (num: number) => 'EVENT!' | 'ODD!';
+export declare const eventOdd: (num: number) => "EVENT!" | "ODD!";
 export declare const isEvent: (num: number) => boolean;
 export declare const isOdd: (num: number) => boolean;
