4ddbf5b8ffb1c901aa3d4ebad9a757523cb139ca
Poafs1 <poafs1@hotmail.com>
2021-08-26 14:19:37 +0700

    First commit

---
+++ dist/index.ts
@@ -0,0 +1,17 @@
+const eventOdd = (num: number) => {
+  if (isEvent(num)) {
+    console.log('Is event number.')
+  } else {
+    console.log('Is odd number.')
+  }
+}
+
+const isEvent = (num: number) => num % 2 == 0
+
+const isOdd = (num: number) => num % 2 != 0
+
+export = {
+  eventOdd: eventOdd,
+  isEvent: isEvent,
+  isOdd: isOdd
+}
